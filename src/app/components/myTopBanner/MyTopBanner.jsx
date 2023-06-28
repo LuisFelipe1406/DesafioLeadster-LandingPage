f16daf880d8f069eeefe06e8f8ec0e2a44591c1e
LuisFelipe1406 <luis_felipes14@hotmail.com>
2023-06-28 20:39:13 -0300

    Feat: add fonts; build MyTopBanner component

--- src/app/components/myTopBanner/MyTopBanner.jsx
+++ src/app/components/myTopBanner/MyTopBanner.jsx
@@ -1,12 +1,23 @@
 import Styles from "./myTopBanner.module.scss";
+import Image from "next/image";
+import assetBanner from "public/img/assetBanner.png";
 
 function MyTopBanner() {
     return (
        <section className={Styles.content}>
-            <div className={Styles.mainInfo}>
-
+            <div className={Styles.bannerButton}>
+                <span><strong>WEBINARS EXCLUSIVOS</strong></span>
             </div>
-            <span>Conheça as estratégias que <strong>mudaram o jogo</strong> e como aplicá-las no seu negócio</span>
+            <section className={Styles.mainInfo}>
+                <h1 className={Styles.subtitle}>Menos Conversinha,</h1>
+                <section className={Styles.titleSection}>
+                    <h1 className={Styles.title}>Mais Conversão</h1> 
+                    <Image className={Styles.icBanner} src={ assetBanner } />
+                </section>
+            </section>
+            <section className={Styles.bottomInfo}>
+                <span>Conheça as estratégias que <strong>mudaram o jogo</strong> e como aplicá-las no seu negócio</span>
+            </section>
        </section> 
     );
 }
